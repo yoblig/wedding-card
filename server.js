e8a5b3ef5302930811009fe46ad9ff61067c9d7a
Kevin Gilboy <gilboyk@gmail.com>
2016-10-18 04:22:09 +0000

    payment feature

--- server.js
+++ server.js
@@ -164,26 +164,88 @@ app.get('/review/:id', function(req, res, next) {
   });
 });
 
-app.get('/order/:id', function(req, res, next) {
+app.get('/order', function(req, res, next) {
+  // app.get('/order/:id', function(req, res, next) {
     res.render('order', {title: 'Order & Pay'});
     
-    var transporter = nodemailer.createTransport('smtps://gilboyk@gmail.com:Go2Hell!@smtp.gmail.com');
-    // var transporter = nodemailer.createTransport('smtps://user%40gmail.com:pass@smtp.gmail.com');
- 
-    // setup e-mail data with unicode symbols 
-    var mailOptions = {
-        from: '"Kev Gilboy" <gilboyk@gmail.com>', // sender address 
-        to: 'kevin_gilboy@yahoo.com', // list of receivers 
-        subject: 'Wedding Card Confirm', // Subject line 
-        text: 'You have cards coming your way', // plaintext body 
-        html: '<b>Get Money</b>' // html body 
-    };
- 
-    // send mail with defined transport object
-    transporter.sendMail(mailOptions, function(error, info) {
-       if(error) return console.log(error);
-       console.log('Message sent: ' + info.response);
+    
+    // Set your secret key: remember to change this to your live secret key in production
+    // See your keys here: https://dashboard.stripe.com/account/apikeys
+    var stripe = require("stripe")("sk_test_BQokikJOvBiI2HlWgH4olfQ2");
+    
+    // Get the credit card details submitted by the form
+    var token = req.body.stripeToken; // Using Express
+    
+    // Create a charge: this will charge the user's card
+    var charge = stripe.charges.create({
+      amount: 4000, // Amount in cents
+      currency: "usd",
+      source: token,
+      description: "Example Card Matrimony Charge"
+    }, function(err, charge) {
+      if (err && err.type === 'StripeCardError') {
+        // The card has been declined
+      }
     });
+    
+  //   Order.findOne({_id: req.params.id}, function(err,order){
+  //   if (err) return console.log(err);
+  //   else {
+      
+  //     var transporter = nodemailer.createTransport('smtps://someone@gmail.com:password@smtp.gmail.com');
+    
+  //     // setup e-mail data with unicode symbols 
+  //     var mailOptions = {
+  //         from: '"', // sender address 
+  //         to: '', // list of receivers 
+  //         subject: 'Wedding Card Confirmation', // Subject line 
+  //         html: 'You have cards coming your way for <b>' + order.bride + '</b>', // html body 
+  //         attachments:   {   // file on disk as an attachment 
+  //           path: 'public/images/cards.png' // stream this file 
+  //         }
+  //     };
+   
+  //     // send mail with defined transport object
+  //     transporter.sendMail(mailOptions, function(error, info) {
+  //       if(error) return console.log(error);
+  //       console.log('Message sent: ' + info.response);
+  //     });
+  //   }
+  // });
+    
+    
+    
+  //   Order.findOne({_id: req.params.id}, function(err,order){
+  //   if (err) return console.log(err);
+  //   else {
+      
+  //     var transporter = nodemailer.createTransport('smtps://gilboyk@gmail.com:Go2Hell!@smtp.gmail.com');
+    
+  //     // setup e-mail data with unicode symbols 
+  //     var mailOptions = {
+  //         from: '"Kevin Gilboy" <gilboyk@gmail.com>', // sender address 
+  //         to: 'kevin_gilboy@yahoo.com', // list of receivers 
+  //         subject: 'Wedding Card Confirm', // Subject line 
+  //         text: 'You have cards coming your way for ' + order.bride, // plaintext body 
+  //         //html: '<b>Get Money</b>' // html body 
+  //     };
+   
+  //     // send mail with defined transport object
+  //     transporter.sendMail(mailOptions, function(error, info) {
+  //       if(error) return console.log(error);
+  //       console.log('Message sent: ' + info.response);
+  //     });
+      
+  //     ejs.renderFile('./views/confirm.ejs', {order:order, title: "moneybank"}, {}, function(err, str){
+  //     if (err) console.log(err);
+  //     else {
+  //       res.send(str);
+  //     }
+  //   });
+  //   }
+  // });
+    
+   
 });
 
 app.get('/confirm', function(req, res, next) {
